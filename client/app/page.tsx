440d150b76b2d085e9f088b642570c821a6f4f71
saintsauceee <t.donghao.zeng@gmail.com>
2026-03-17 00:48:24 -0400

    setup dashboard

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,26 @@ import MapWrapper from "./components/MapWrapper";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
-      <MapWrapper />
+    <div className="w-screen h-screen grid grid-cols-2 grid-rows-2">
+      {/* Top-left: Map */}
+      <div className="relative">
+        <MapWrapper />
+      </div>
+
+      {/* Top-right */}
+      <div className="bg-neutral-900 border-l border-neutral-700 flex items-center justify-center">
+        <span className="text-neutral-600 text-sm">top right</span>
+      </div>
+
+      {/* Bottom-left */}
+      <div className="bg-neutral-900 border-t border-neutral-700 flex items-center justify-center">
+        <span className="text-neutral-600 text-sm">bottom left</span>
+      </div>
+
+      {/* Bottom-right */}
+      <div className="bg-neutral-900 border-t border-l border-neutral-700 flex items-center justify-center">
+        <span className="text-neutral-600 text-sm">bottom right</span>
+      </div>
     </div>
   );
 }
